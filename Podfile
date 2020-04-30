021b8aaef25d76341054d9858e3ee43f7ffecc01
Tasos Tsolis <tasos.tsolis@gmail.com>
2020-04-30 16:58:45 +0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,21 @@
+platform :ios, '10.0'
+
+target 'TestProject' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'SDWebImage'
+
+
+
+
+
+  target 'TestProjectTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TestProjectUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Tasos Tsolis on 30/4/20.
+//  Copyright © 2020 Tsolis Tasos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
