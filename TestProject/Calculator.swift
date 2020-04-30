6b6d7b8cfc8143adc5d83ff606e996452819c0d9
Tasos Tsolis <tasos.tsolis@gmail.com>
2020-04-30 17:28:50 +0300

    Create a calculator object

---
+++ TestProject/Calculator.swift
@@ -0,0 +1,16 @@
+//
+//  Calculator.swift
+//  TestProject
+//
+//  Created by Tasos Tsolis on 30/4/20.
+//  Copyright © 2020 Tsolis Tasos. All rights reserved.
+//
+
+import Foundation
+
+struct Calculator {
+
+    static func add(number:Int, toNumber: Int) -> Int {
+        number + toNumber
+    }
+}
---
+++ TestProjectTests/CalculatorTests.swift
@@ -0,0 +1,27 @@
+//
+//  CalculatorTests.swift
+//  TestProjectTests
+//
+//  Created by Tasos Tsolis on 30/4/20.
+//  Copyright © 2020 Tsolis Tasos. All rights reserved.
+//
+
+import XCTest
+@testable import TestProject
+
+class CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testCalculator() throws {
+
+        let finalNumber = Calculator.add(number: 10, toNumber: 10)
+        XCTAssertEqual(finalNumber, 20)
+    }
+}
